b23645c468d42c7b3556638ebd57e1ba15eec6ff
MJackson22-bit <jm5444859@gmail.com>
2023-04-13 11:24:29 -0600

    Add Network Module to request data

--- app/src/main/java/com/example/rickmortygraphql/core/di/NetworkModule.kt
+++ app/src/main/java/com/example/rickmortygraphql/core/di/NetworkModule.kt
@@ -1,11 +1,39 @@
 package com.example.rickmortygraphql.core.di
 
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.network.okHttpClient
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .callTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApolloClient(okHttpClient: OkHttpClient) : ApolloClient {
+        return ApolloClient.Builder()
+            .serverUrl("https://rickandmortyapi.com/graphql")
+            .okHttpClient(okHttpClient)
+            .build()
+    }
 }
